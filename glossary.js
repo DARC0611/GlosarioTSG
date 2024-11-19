b87d54f50a756b82c5fc1bf4c6454a618746a956
DARC0611 <diegoruiz.ceo@gmail.com>
2024-11-19 01:18:52 -0500

    Update glossary.js

--- glossary.js
+++ glossary.js
@@ -1,86 +1,107 @@
 // Glossary data
 const glossaryItems = [
     {
+        title: 'Ingeniería',
+        description: 'Disciplina que aplica principios científicos y matemáticos para diseñar, construir y optimizar sistemas, estructuras y dispositivos para resolver problemas prácticos. Ejemplo: Diseñar un puente para resistir terremotos.',
+        image: 'Ingeniería.jpg'
+    },
+    {
         title: 'Sistema',
-        description: 'Conjunto de partes coordinadas y en interacción para alcanzar un conjunto de objetivos.',
-        image: 'sistema.png'
+        description: 'Conjunto de elementos interrelacionados que trabajan juntos para cumplir un propósito específico. Ejemplo: Un motor de combustión interna que convierte energía química en movimiento.',
+        image: 'sistema.jpg'
     },
     {
-        title: 'Sistema Abierto',
-        description: 'Un sistema que intercambia materia, energía e información con su entorno (El ecosistema de una selva tropical).',
-        image: 'sistemaAbierto.png'
+        title: 'Prototipo',
+        description: 'Modelo preliminar o versión inicial de un producto que se utiliza para probar y validar ideas antes de la producción final. Ejemplo: Un prototipo de un dron para pruebas de vuelo.',
+        image: 'prototipo.jpg'
     },
     {
-        title: 'Sistema Cerrado',
-        description: 'No intercambia la energía con su medio (Ya sea importación como exportación) ejemplo: Reloj de cuerda',
-        image: 'sistemaCerrado.png'
+        title: 'Automatización',
+        description: 'Uso de tecnologías y sistemas para realizar tareas con poca o ninguna intervención humana, aumentando la eficiencia y precisión. Ejemplo: Una línea de ensamblaje robotizada en una fábrica de automóviles.',
+        image: 'automatizacion.jpg'
     },
     {
-        title: 'Sistema Natural',
-        description: 'Es un sistema interconectado de elementos y procesos que interactúan de manera dinámica con la naturaleza. Ejemplo (El cuerpo Humano).',
-        image: 'sistemaNatural.png'
+        title: 'Sensores',
+        description: 'Dispositivos que detectan cambios físicos o químicos en el entorno y los convierten en señales para ser procesadas. Ejemplo: Un sensor de temperatura que mide el calor en un horno.',
+        image: 'Sensores.jpg'
     },
     {
-        title: 'Sistema Artificial',
-        description: 'Este sistema fue creado por el ser humano y sirve a lo contrario del sistema natural ya que se originan de la acción humana. Ejemplo (Una computadora).',
-        image: 'sistemaArtificial.png'
+        title: 'Actuadores',
+        description: 'Componentes mecánicos que convierten señales eléctricas en movimiento físico. Ejemplo: Un servomotor utilizado para mover el brazo de un robot.',
+        image: 'actuadores.png'
     },
     {
-        title: 'Sistema Determinístico',
-        description: 'En este sistema el azar no está involucrado en el desarrollo de los futuros estados del sistema. Ejemplo (Máquina Turing).',
-        image: 'sistemaDeterministico.png'
+        title: 'Control',
+        description: 'Proceso de regulación de un sistema para mantener un comportamiento deseado. Ejemplo: Usar un controlador PID para mantener la velocidad constante de un motor.',
+        image: 'control.jpg'
     },
     {
-        title: 'Sistema Probabilístico',
-        description: 'Se basa en la teoría de la probabilidad o en la aleatoriedad para predecir eventos futuros. Ejemplo (Lotería).',
-        image: 'sistemaProbabilistico.png'
+        title: 'Diseño',
+        description: 'Proceso creativo para idear soluciones innovadoras, que incluye la planificación, bocetación y modelado de productos o sistemas. Ejemplo: Diseñar el chasis de un automóvil deportivo.',
+        image: 'diseño.jpg'
     },
     {
-        title: 'GESTALT',
-        description: 'Una forma de organización donde el todo es más que la suma de sus partes. Como los rostros humanos, que tendemos a ver como un todo, aunque estén compuestos de partes distintas, como ojos, nariz, boca, etc.',
-        image: 'GESTALT.png'
+        title: 'Cinemática',
+        description: 'Rama de la mecánica que estudia el movimiento de los cuerpos sin considerar las fuerzas que lo producen. Ejemplo: Analizar el movimiento de una pelota lanzada al aire.',
+        image: 'cinematica.jpg'
     },
     {
-        title: 'Equifinalidad',
-        description: 'Es un concepto que sostiene que, en sistemas dinámicos y complejos, múltiples trayectorias o condiciones iniciales pueden converger en un mismo estado o resultado final. Ejemplo (Formación de un bosque).',
-        image: 'equinifinidad.png'
+        title: 'Dinámica',
+        description: 'Estudio de las fuerzas y su efecto en el movimiento de los objetos. Ejemplo: Calcular la fuerza necesaria para acelerar un vehículo de 0 a 100 km/h.',
+        image: 'dinamica.png'
     },
     {
-        title: 'Sinergia',
-        description: 'Acción de dos o más causas cuyo efecto es superior a la suma de los efectos individuales. Ejemplo (El conjunto del gusto y el olfato para saborear alimentos).',
-        image: 'sinergia.png'
+        title: 'CAD (Diseño Asistido por Computadora)',
+        description: 'Herramientas de software utilizadas para diseñar y modelar componentes y sistemas en 2D o 3D. Ejemplo: Crear un diseño de un engranaje usando AutoCAD.',
+        image: 'CAD.jpg'
     },
     {
-        title: 'Recursividad',
-        description: 'Podemos entender por recursividad el hecho de que un objeto sinergético, un sistema, esté compuesto de partes con características tales que son a su vez objetos sinergéticos (Sistemas). Ejemplo la factorial de un número.',
-        image: 'recursividad.png'
+        title: 'Electromecánica',
+        description: 'Integración de componentes eléctricos y mecánicos para crear sistemas híbridos. Ejemplo: Un robot que combina motores eléctricos con engranajes mecánicos.',
+        image: 'electromecanica.png'
     },
     {
-        title: 'Morfogénesis',
-        description: 'El proceso o desarrollo de una forma o Estructura. Ejemplo (Copo de nieve)',
-        image: 'morfogenesis.png'
+        title: 'Robótica',
+        description: 'Rama de la ingeniería que se ocupa del diseño, construcción y operación de robots. Ejemplo: Diseñar un robot autónomo para entregar paquetes.',
+        image: 'Robotica.png'
     },
     {
-        title: 'Organicidad',
-        description: 'Es un sistema constituido por partes que forman un conjunto coherente. Un ejemplo puede ser los sistemas vivos ya que en general poseen una característica que los lleva no solo a permanecer (o sobrevivir) sino a crecer y expandirse. Ejemplo (Naturaleza)',
-        image: 'organicidad.png'
+        title: 'Microcontrolador',
+        description: 'Pequeño dispositivo computacional programable que actúa como el cerebro de sistemas embebidos. Ejemplo: Un Arduino que controla la iluminación de una casa inteligente.',
+        image: 'microcontrolador.png'
     },
     {
-        title: 'Isomorfismo',
-        description: 'La relación entre dos sistemas que tienen una misma estructura.',
-        image: 'isoformismo.png'
+        title: 'Mecatrónica',
+        description: 'Disciplina multidisciplinaria que combina mecánica, electrónica, informática y control para diseñar sistemas inteligentes. Ejemplo: Un brazo robótico usado en cirugía asistida.',
+        image: 'mecatronica.jpg'
     },
     {
-        title: 'Homeomorfismo',
-        description: 'Una relación de similitud estructural entre dos sistemas, aunque no necesariamente idéntica.',
-        image: 'homeoformismo.png'
+        title: 'Ciclo de vida del producto',
+        description: 'Serie de etapas por las que pasa un producto, desde su concepción y desarrollo hasta su uso y eventual eliminación. Ejemplo: Desarrollo de un teléfono móvil desde su diseño hasta su reciclaje.',
+        image: 'ciclodevida.png'
     },
     {
-        title: 'Homeostasis',
-        description: 'Sirve para referirse a la capacidad de ciertos programas o sistemas cibernéticos para autorregular algunas de sus funciones y mantener el equilibrio entre las interrelaciones de sus diversos componentes. Ejemplo (Temperatura del cuerpo Humano)',
-        image: 'homeostasis.png'
+        title: 'Energía',
+        description: 'Capacidad para realizar trabajo; en ingeniería, se estudian diversas formas como mecánica, eléctrica o térmica. Ejemplo: Energía solar captada para alimentar un sistema de riego.',
+        image: 'energia.png'
     },
-]
+    {
+        title: 'Retroalimentación (Feedback)',
+        description: 'Proceso mediante el cual un sistema ajusta su comportamiento en función de los resultados obtenidos. Ejemplo: Un termostato ajustando la temperatura de una habitación.',
+        image: 'retroalimentacion.png'
+    },
+    {
+        title: 'Modelado',
+        description: 'Representación matemática o física de un sistema para analizar y predecir su comportamiento. Ejemplo: Modelar el flujo de aire en el diseño de un ala de avión.',
+        image: 'modelado.jpg'
+    },
+    {
+        title: 'Sostenibilidad',
+        description: 'Principio de diseñar sistemas y productos que minimicen el impacto ambiental y promuevan un uso eficiente de los recursos. Ejemplo: Diseño de edificios con paneles solares y sistemas de reciclaje de agua.',
+        image: 'sostenibilidad.jpg'
+    }
+    
+];
 
 
 document.addEventListener('DOMContentLoaded', () => {
